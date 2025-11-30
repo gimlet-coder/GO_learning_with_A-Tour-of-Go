6713ccc03939b6e89279c5b1875177490c12e255
gimlet-coder <riffle3580@gmail.com>
2025-12-01 04:01:41 +0900

    New

---
+++ 01_Basics/More_types_structs_slices_and_maps/p05_Struct_Literals/struct-literals.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2}
+	// 下のように X: 1, Y: 2 としても良いし, 省略もできる
+	v2 = Vertex{X: 1}
+	/* X に 1 が入るが Y が省略されている場合, 暗黙的に Y : 0 が設定される
+	ただし, 上のように X: を省略してしまうと X にかかっているのか Y にかかっているのかわからない上に引数の数も一致しないのでエラーとなる
+	*/
+	v3 = Vertex{}
+	// この場合, 各成分 X, Y は 0 となる
+	p = &Vertex{1, 2}
+	// 構造体のポインタも渡すことができる
+)
+
+func main() {
+	fmt.Println(v1, p.X, v2, v3)
+}
